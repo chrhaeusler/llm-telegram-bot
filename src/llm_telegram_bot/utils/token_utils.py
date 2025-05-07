1091ca7fbf57db354285b3b4a97ff5e1ebc293e7
Christian O. Häusler <der.haeusler@gmx.net>
2025-05-07 15:06:53 +0200

    fix: switch from tiktokens to words (momentarily) v2

--- src/llm_telegram_bot/utils/token_utils.py
+++ src/llm_telegram_bot/utils/token_utils.py
@@ -2,13 +2,14 @@
 
 import re
 
-import tiktoken
+# import tiktoken
 
 
 def count_tokens_simple(text: str) -> int:
     # very rough: split on whitespace + punctuation
     return len(re.findall(r"\w+|[^\s\w]", text))
 
-def count_tokens(text: str, model: str = "gpt-3.5-turbo") -> int:
-    enc = tiktoken.encoding_for_model(model)
-    return len(enc.encode(text))
+
+# def count_tokens(text: str, model: str = "gpt-3.5-turbo") -> int:
+#     enc = tiktoken.encoding_for_model(model)
+#     return len(enc.encode(text))
